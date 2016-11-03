d6430d4ea2a0ef475f9aa21521cb865798905c34
alex6235 <alex.cheng@advantech.com.tw>
2016-11-03 17:47:27 +0800

    0023_ANDROID_001_modify_uboot_setenv_parameter

--- common/image-android.c
+++ common/image-android.c
@@ -79,13 +79,8 @@ int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 
 	char newbootargs[512] = {0};
 	char commandline[1024] = {0};
-#ifdef CONFIG_ANDROID_SUPPORT
 	char *bootargs = getenv("bootargs");
-	char *adv_cmd_line = getenv("bootargs_adv");
-	sprintf(bootargs, "%s %s", bootargs, adv_cmd_line);
-#else
-	char *bootargs = getenv("bootargs");
-#endif
+	
 
 	if (bootargs) {
 		strcpy(commandline, bootargs);
@@ -97,8 +92,17 @@ int android_image_get_kernel(const struct andr_img_hdr *hdr, int verify,
 	struct tag_serialnr serialnr;
 	get_board_serial(&serialnr);
 
+#ifdef CONFIG_ANDROID_SUPPORT
+	char *adv_cmd_line = getenv("bootargs_adv");
+#endif
+
 	sprintf(newbootargs,
+#ifdef CONFIG_ANDROID_SUPPORT
+					" %s androidboot.serialno=%08x%08x",
+					adv_cmd_line,
+#else
 					" androidboot.serialno=%08x%08x",
+#endif
 					serialnr.high,
 					serialnr.low);
 	strcat(commandline, newbootargs);
