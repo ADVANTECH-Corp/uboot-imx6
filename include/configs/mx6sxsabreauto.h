nxp-imx/uboot-imx

MLK-12527-3 android: Add board support to enable android fastboot
Add board level support for android fastboot feature. Each board has
a android specified header file for defining android related configuraitons.
And add build targets for their android uboot images building.

For mx6qsabreauto, mx6sabresd and mx7dsabresd, we enable the android
fastboot exclusive with DFU.

Signed-off-by: Ye Li <ye.li@nxp.com>

include/configs/mx6sxsabreauto.h
@@ -345,4 +345,8 @@
 #endif
 #endif
 
+#if defined(CONFIG_ANDROID_SUPPORT)
+#include "mx6sxsabreautoandroid.h"
+#endif
+
 #endif				/* __CONFIG_H */

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 43fe988af28c5e51fb23aa846e04bc9698256926)

include/configs/mx6sxsabreauto.h
@@ -318,4 +318,8 @@
 #endif
 #endif
 
+#if defined(CONFIG_FASTBOOT)
+#include "mx6sxsabreautoandroid.h"
+#endif
+
 #endif				/* __CONFIG_H */