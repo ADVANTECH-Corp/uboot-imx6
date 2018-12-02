nxp-imx/uboot-imx

MA-9213 imx: mx7ulp-evk: Add android support
Add android features on i.MX7ULP EVK board.
Implement the code to get boot device and the serial number on mx7ulp.
TODO: will add the code which check misc partition after porting BCB.

Change-Id: I9d06fecba303fa4dfdcaf73da1b6246444697bba
Signed-off-by: Sanshan Zhang <sanshan.zhang@nxp.com>

include/configs/mx7ulp_evk.h
@@ -293,5 +293,8 @@
 #endif
 
 #define CONFIG_OF_SYSTEM_SETUP
+#if defined(CONFIG_ANDROID_SUPPORT)
+#include "mx7ulp_evk_android.h"
+#endif
 
 #endif	/* __CONFIG_H */

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 4c60cba3a017b921aebb84dd1268c898e549c99a)
Signed-off-by: Ye Li <ye.li@nxp.com>

include/configs/mx7ulp_evk.h
@@ -248,4 +248,8 @@
 
 #define CONFIG_OF_SYSTEM_SETUP
 
+#if defined(CONFIG_FASTBOOT)
+#include "mx7ulp_evk_android.h"
+#endif
+
 #endif	/* __CONFIG_H */