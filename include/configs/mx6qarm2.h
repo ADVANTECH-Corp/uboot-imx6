nxp-imx/uboot-imx

MLK-13130: configs: mx6qarm2: android lpddr2 pop support
Add Android support for mx6qarm2 lpddr2 pop target

Signed-off-by: Adrian Alonso <adrian.alonso@nxp.com>

include/configs/mx6qarm2.h
@@ -176,4 +176,8 @@
 #define CONFIG_USB_MAX_CONTROLLER_COUNT	1
 #endif
 
-#endif				/* __CONFIG_H */
+#if defined(CONFIG_ANDROID_SUPPORT)
+#include "mx6qarm2_android.h"
+#endif
+
+#endif                         /* __CONFIG_H */

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 6356f2b420f3571493755f6b3a307a66a539b60c)

include/configs/mx6qarm2.h
@@ -152,4 +152,8 @@
 #define CONFIG_USB_MAX_CONTROLLER_COUNT	1
 #endif
 
-#endif				/* __CONFIG_H */
+#if defined(CONFIG_FASTBOOT)
+#include "mx6qarm2_android.h"
+#endif
+
+#endif                         /* __CONFIG_H */