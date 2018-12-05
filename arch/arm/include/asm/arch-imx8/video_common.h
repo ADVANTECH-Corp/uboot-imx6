nxp-imx/uboot-imx

MLK-15324-3 imx8: Add LVDS and DC setup common functions
Add common functions for LVDS/DC setup, video framebuffer init/disable,
LVDS to HDMI card settings, etc. Refactor it from video_imxdpuv1.c.
1. Add power, clocks, PLL relevant setup for LVDS and DC.
2. Configure the LVDS and its PHY settings for the display format and pixel link.
3. Setup the LVDS to HDMI card.
4. Implement the video_hw_init by calling DC driver API to output data to
   specified display panel.

Signed-off-by: Oliver Brown <oliver.brown@nxp.com>
Signed-off-by: Ye Li <ye.li@nxp.com>

arch/arm/include/asm/arch-imx8/video_common.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ */
+
+#ifndef __ASM_ARCH_VIDEO_COMMON_H__
+#define __ASM_ARCH_VIDEO_COMMON_H__
+
+#include <asm/imx-common/sci/sci.h>
+#include <linux/fb.h>
+
+#define	PS2KHZ(ps)	(1000000000UL / (ps))
+
+int lvds2hdmi_setup(int i2c_bus);
+int lvds_soc_setup(int lvds_id, sc_pm_clock_rate_t pixel_clock);
+void lvds_configure(int lvds_id);
+int display_controller_setup(sc_pm_clock_rate_t pixel_clock);
+int imxdpuv1_fb_init(struct fb_videomode const *mode, uint8_t disp, uint32_t pixfmt);
+void imxdpuv1_fb_disable(void);
+
+#endif /* __ASM_ARCH_VIDEO_COMMON_H__ */

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 9317f7ea4d976335a633f7056bbac1f77b01247a)

arch/arm/include/asm/arch-imx8/video_common.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ */
+
+#ifndef __ASM_ARCH_VIDEO_COMMON_H__
+#define __ASM_ARCH_VIDEO_COMMON_H__
+
+#include <asm/mach-imx/sci/sci.h>
+#include <linux/fb.h>
+
+#define	PS2KHZ(ps)	(1000000000UL / (ps))
+
+int lvds2hdmi_setup(int i2c_bus);
+int lvds_soc_setup(int lvds_id, sc_pm_clock_rate_t pixel_clock);
+void lvds_configure(int lvds_id);
+int display_controller_setup(sc_pm_clock_rate_t pixel_clock);
+int imxdpuv1_fb_init(struct fb_videomode const *mode, uint8_t disp, uint32_t pixfmt);
+void imxdpuv1_fb_disable(void);
+
+#endif /* __ASM_ARCH_VIDEO_COMMON_H__ */