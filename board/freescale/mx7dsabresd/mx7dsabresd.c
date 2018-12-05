nxp-imx/uboot-imx

MLK-16753-9 imx: mx7: add optee support
Add different defconfigs.
Enable Trustzone.
Update env to runtime boot OP-TEE.
mx7d arm2 board not supported now.

Signed-off-by: Peng Fan <peng.fan@nxp.com>

board/freescale/mx7dsabresd/mx7dsabresd.c
@@ -648,6 +648,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

board/freescale/mx7ulp_evk/mx7ulp_evk.c
@@ -207,6 +207,11 @@ int board_mmc_get_env_dev(int devno)
 
 int board_late_init(void)
 {
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 435807b41ae99bb9f4e0b2c7b9e5287c2bd520c0)

board/freescale/mx7dsabresd/mx7dsabresd.c
@@ -669,6 +669,11 @@ int board_late_init(void)
 	add_board_boot_modes(board_boot_modes);
 #endif
 
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

board/freescale/mx7ulp_evk/mx7ulp_evk.c
@@ -198,6 +198,11 @@ int board_mmc_get_env_dev(int devno)
 
 int board_late_init(void)
 {
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif