nxp-imx/uboot-imx

MLK-18431-01 imx8mm: move the spl bss base to ocram 0x910000
Move the SPL bss and malloc base to OCRAM 0x910000.
OCRAM_S will be reserved for dram low power purpose.

Signed-off-by: Bai Ping <ping.bai@nxp.com>

include/configs/imx8mm_val.h
@@ -30,14 +30,14 @@
 #define CONFIG_SPL_I2C_SUPPORT
 #define CONFIG_SPL_BOARD_INIT
 #define CONFIG_SPL_LDSCRIPT		"arch/arm/cpu/armv8/u-boot-spl.lds"
-#define CONFIG_SPL_STACK		0x187FF0
+#define CONFIG_SPL_STACK		0x91FFF0
 #define CONFIG_SPL_LIBCOMMON_SUPPORT
 #define CONFIG_SPL_LIBGENERIC_SUPPORT
 #define CONFIG_SPL_SERIAL_SUPPORT
 #define CONFIG_SPL_GPIO_SUPPORT
-#define CONFIG_SPL_BSS_START_ADDR      0x00180000
+#define CONFIG_SPL_BSS_START_ADDR      0x00910000
 #define CONFIG_SPL_BSS_MAX_SIZE        0x2000	/* 8 KB */
-#define CONFIG_SYS_SPL_MALLOC_START    0x00182000
+#define CONFIG_SYS_SPL_MALLOC_START    0x00912000
 #define CONFIG_SYS_SPL_MALLOC_SIZE     0x2000	/* 8 KB */
 #define CONFIG_SYS_ICACHE_OFF
 #define CONFIG_SYS_DCACHE_OFF

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 2b2cd1adc23525d2fb79dc281cd2a741ebe13f90)

include/configs/imx8mm_val.h
@@ -28,14 +28,14 @@
 #define CONFIG_SPL_DRIVERS_MISC_SUPPORT
 #define CONFIG_SPL_I2C_SUPPORT
 #define CONFIG_SPL_LDSCRIPT		"arch/arm/cpu/armv8/u-boot-spl.lds"
-#define CONFIG_SPL_STACK		0x187FF0
+#define CONFIG_SPL_STACK		0x91FFF0
 #define CONFIG_SPL_LIBCOMMON_SUPPORT
 #define CONFIG_SPL_LIBGENERIC_SUPPORT
 #define CONFIG_SPL_SERIAL_SUPPORT
 #define CONFIG_SPL_GPIO_SUPPORT
-#define CONFIG_SPL_BSS_START_ADDR      0x00180000
+#define CONFIG_SPL_BSS_START_ADDR      0x00910000
 #define CONFIG_SPL_BSS_MAX_SIZE        0x1000	/* 4 KB */
-#define CONFIG_SYS_SPL_MALLOC_START    0x00181000
+#define CONFIG_SYS_SPL_MALLOC_START    0x00911000
 #define CONFIG_SYS_SPL_MALLOC_SIZE     0x3000	/* 12 KB */
 #define CONFIG_SYS_ICACHE_OFF
 #define CONFIG_SYS_DCACHE_OFF