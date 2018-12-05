nxp-imx/uboot-imx

MLK-14938-8 imx8: Add SMMU setup to Soc codes
We setup SMMU in arch_preboot_os to avoid breaking u-boot driver.
Add a kconfig entry CONFIG_IMX_SMMU to enable it.
So far, the USDHC0-USDHC1 and FEC0-FEC1 are added into sid.

Signed-off-by: Peng Fan <peng.fan@nxp.com>
Signed-off-by: Ye Li <ye.li@nxp.com>

arch/arm/include/asm/arch-imx8/sid.h
@@ -1,0 +1,14 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#include <asm/imx-common/sci/sci.h>
+
+struct smmu_sid {
+	sc_rsrc_t rsrc;
+	sc_rm_sid_t sid;
+	char dev_name[32];
+};
+
+sc_err_t imx8_config_smmu_sid(struct smmu_sid *dev_sids, int size);

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 7da6345919ee59a26cf65b4bc29072eea2fc0909)

arch/arm/include/asm/arch-imx8/sid.h
@@ -1,0 +1,14 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+#include <asm/mach-imx/sci/sci.h>
+
+struct smmu_sid {
+	sc_rsrc_t rsrc;
+	sc_rm_sid_t sid;
+	char dev_name[32];
+};
+
+sc_err_t imx8_config_smmu_sid(struct smmu_sid *dev_sids, int size);