nxp-imx/uboot-imx

MLK-14938-6 imx8: Add fuse driver to wrap SCFW API
Implement a fuse driver to wrap the SCFW OCOTP API and provide interfaces
to u-boot fuse command. So that we can use "fuse read" or "fuse sense" command.

Since there is no concept of fuse bank on i.MX8. Need set "bank" parameter to 0
when using the fuse command.

Signed-off-by: Ye Li <ye.li@nxp.com>

arch/arm/cpu/armv8/imx8/fuse.c
@@ -1,0 +1,52 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ */
+
+#include <common.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <fuse.h>
+#include <asm/imx-common/sci/sci.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int fuse_read(u32 bank, u32 word, u32 *val)
+{
+	return fuse_sense(bank, word, val);
+}
+
+int fuse_sense(u32 bank, u32 word, u32 *val)
+{
+	sc_err_t err;
+	sc_ipc_t ipc;
+
+	if (bank != 0) {
+		printf("Invalid bank argument, ONLY bank 0 is supported\n");
+		return -EINVAL;
+	}
+
+	ipc = gd->arch.ipc_channel_handle;
+
+	err = sc_misc_otp_fuse_read(ipc, word, val);
+	if (err != SC_ERR_NONE) {
+		printf("fuse read error: %d\n", err);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+int fuse_prog(u32 bank, u32 word, u32 val)
+{
+	printf("Program fuse to i.MX8 in u-boot is forbidden\n");
+	return -EPERM;
+}
+
+int fuse_override(u32 bank, u32 word, u32 val)
+{
+	printf("Override fuse to i.MX8 in u-boot is forbidden\n");
+	return -EPERM;
+}

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 2c8b160f9c5b96434bce87108f82b1bb434abaa5)

arch/arm/mach-imx/imx8/fuse.c
@@ -1,0 +1,52 @@
+/*
+ * Copyright 2017 NXP
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ *
+ */
+
+#include <common.h>
+#include <errno.h>
+#include <asm/io.h>
+#include <fuse.h>
+#include <asm/mach-imx/sci/sci.h>
+
+DECLARE_GLOBAL_DATA_PTR;
+
+int fuse_read(u32 bank, u32 word, u32 *val)
+{
+	return fuse_sense(bank, word, val);
+}
+
+int fuse_sense(u32 bank, u32 word, u32 *val)
+{
+	sc_err_t err;
+	sc_ipc_t ipc;
+
+	if (bank != 0) {
+		printf("Invalid bank argument, ONLY bank 0 is supported\n");
+		return -EINVAL;
+	}
+
+	ipc = gd->arch.ipc_channel_handle;
+
+	err = sc_misc_otp_fuse_read(ipc, word, val);
+	if (err != SC_ERR_NONE) {
+		printf("fuse read error: %d\n", err);
+		return -EIO;
+	}
+
+	return 0;
+}
+
+int fuse_prog(u32 bank, u32 word, u32 val)
+{
+	printf("Program fuse to i.MX8 in u-boot is forbidden\n");
+	return -EPERM;
+}
+
+int fuse_override(u32 bank, u32 word, u32 val)
+{
+	printf("Override fuse to i.MX8 in u-boot is forbidden\n");
+	return -EPERM;
+}