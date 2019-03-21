alejandrolozano2/GSOM

MA-12283 Enter fastboot mode directly if usb boot
Change bootcmd to "fastboot 0" directly when detect
booted from USB. By this the u-boot which booted by
uuu will enter fastboot mode and ready for the
remain commands.

Signed-off-by: Haoran.Wang <elven.wang@nxp.com>

drivers/usb/gadget/f_fastboot.c
@@ -1544,6 +1544,10 @@ void board_fastboot_setup(void)
 		if (!getenv("bootcmd"))
 			setenv("bootcmd", boot_dev_part);
 		break;
+	case USB_BOOT:
+		printf("Detect USB boot. Will enter fastboot mode!\n");
+		if (!getenv("bootcmd"))
+			setenv("bootcmd", "fastboot 0");
 #endif /*CONFIG_FASTBOOT_STORAGE_MMC*/
 	default:
 		printf("unsupported boot devices\n");

ADVANTECH-Corp/uboot-imx6

Change-Id: I937c07f770e68568dcb3ea67130ee6634feea47a

drivers/usb/gadget/f_fastboot.c
@@ -1559,6 +1559,10 @@ void board_fastboot_setup(void)
 		if (!env_get("bootcmd"))
 			env_set("bootcmd", boot_dev_part);
 		break;
+	case USB_BOOT:
+		printf("Detect USB boot. Will enter fastboot mode!\n");
+		if (!env_get("bootcmd"))
+			env_set("bootcmd", "fastboot 0");
 #endif /*CONFIG_FASTBOOT_STORAGE_MMC*/
 	default:
 		printf("unsupported boot devices\n");