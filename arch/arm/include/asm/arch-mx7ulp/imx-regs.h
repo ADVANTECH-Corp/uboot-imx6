nxp-imx/uboot-imx

MLK-13929-4 mx7ulp: Update registers and memory map for DSI and LCDIF
Update the registers base address and LCDIF registers structure for
mx7ulp.

Signed-off-by: Ye Li <ye.li@nxp.com>

arch/arm/include/asm/arch-mx7ulp/imx-regs.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * SPDX-License-Identifier: GPL-2.0+
  */
@@ -65,6 +66,8 @@
 #define MMDC0_AIPS3_SLOT			(43)
 #define IOMUXC_DDR_AIPS3_SLOT		(45)
 #define MMDC0_PCC3_SLOT				(43)
+#define DSI_AIPS3_SLOT				(41)
+#define LCDIF_AIPS3_SLOT			(42)
 
 #define LPI2C0_AIPS0_SLOT			(51)
 #define LPI2C1_AIPS0_SLOT			(52)
@@ -185,6 +188,10 @@
 
 #define USDHC0_RBASE            ((AIPS2_BASE + (AIPS2_SLOT_SIZE * USDHC0_AIPS2_SLOT)))
 #define USDHC1_RBASE            ((AIPS2_BASE + (AIPS2_SLOT_SIZE * USDHC1_AIPS2_SLOT)))
+
+#define DSI_RBASE				((AIPS3_BASE + (AIPS3_SLOT_SIZE * DSI_AIPS3_SLOT)))
+#define LCDIF_RBASE				((AIPS3_BASE + (AIPS3_SLOT_SIZE * LCDIF_AIPS3_SLOT)))
+#define MXS_LCDIF_BASE		LCDIF_RBASE
 
 #define SNVS_BASE		((AIPS2_BASE + (AIPS2_SLOT_SIZE * SNVS_AIPS2_SLOT)))
 #define SNVS_LP_LPCR		(SNVS_BASE + 0x38)
@@ -975,6 +982,7 @@
 #define MMDC_MPWRDQBY3DL_WR_DQ25_DEL_MASK      ((0x3f  << MMDC_MPWRDQBY3DL_WR_DQ25_DEL))
 #define MMDC_MPWRDQBY3DL_WR_DQ24_DEL_MASK      ((0x3f  << MMDC_MPWRDQBY3DL_WR_DQ24_DEL))
 
+#include <asm/imx-common/regs-lcdif.h>
 
 #if !(defined(__KERNEL_STRICT_NAMES) || defined(__ASSEMBLY__))
 

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 29a2032fc0c2330718dbab1f96c1201ae5b49b6f)
(cherry picked from commit d9c18658e20ad8cca6f1d1fda39c2c0b8f4fed95)

arch/arm/include/asm/arch-mx7ulp/imx-regs.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * SPDX-License-Identifier: GPL-2.0+
  */
@@ -64,6 +65,8 @@
 #define SIM1_PCC1_SLOT			(48)
 #define MMDC0_AIPS3_SLOT		(43)
 #define IOMUXC_DDR_AIPS3_SLOT		(45)
+#define DSI_AIPS3_SLOT			(41)
+#define LCDIF_AIPS3_SLOT		(42)
 
 #define LPI2C0_AIPS0_SLOT		(51)
 #define LPI2C1_AIPS0_SLOT		(52)
@@ -176,6 +179,10 @@
 
 #define USDHC0_RBASE	((AIPS2_BASE + (AIPS2_SLOT_SIZE * USDHC0_AIPS2_SLOT)))
 #define USDHC1_RBASE	((AIPS2_BASE + (AIPS2_SLOT_SIZE * USDHC1_AIPS2_SLOT)))
+
+#define DSI_RBASE	((AIPS3_BASE + (AIPS3_SLOT_SIZE * DSI_AIPS3_SLOT)))
+#define LCDIF_RBASE	((AIPS3_BASE + (AIPS3_SLOT_SIZE * LCDIF_AIPS3_SLOT)))
+#define MXS_LCDIF_BASE	LCDIF_RBASE
 
 #define SNVS_BASE	((AIPS2_BASE + (AIPS2_SLOT_SIZE * SNVS_AIPS2_SLOT)))
 #define SNVS_LP_LPCR	(SNVS_BASE + 0x38)
@@ -944,6 +951,8 @@
 
 #define SNVS_LPCR_DPEN				(0x20)
 #define SNVS_LPCR_SRTC_ENV			(0x1)
+
+#include <asm/mach-imx/regs-lcdif.h>
 
 #if !(defined(__KERNEL_STRICT_NAMES) || defined(__ASSEMBLY__))
 