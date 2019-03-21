nxp-imx/uboot-imx

MA-12196 [Android] system can't enter recovery mode in android auto
CONFIG_CMD_RUN is set to n which do not suppert "run".
use getenv to pass boot cmd to bootcmd

Change-Id: I3866786758b8dd16b2685ec157e8322daa304374
Signed-off-by: zhang sanshan <sanshan.zhang@nxp.com>

drivers/usb/gadget/f_fastboot.c
@@ -1628,7 +1628,7 @@ void board_recovery_setup(void)
 	}
 #endif /* CONFIG_AVB_SUPPORT */
 	printf("setup env for recovery..\n");
-	setenv("bootcmd", "run bootcmd_android_recovery");
+	setenv("bootcmd", getenv("bootcmd_android_recovery"));
 }
 #endif /*CONFIG_ANDROID_RECOVERY*/
 #endif /*CONFIG_FSL_FASTBOOT*/

ADVANTECH-Corp/uboot-imx6

Conflicts:
	drivers/usb/gadget/f_fastboot.c

drivers/usb/gadget/f_fastboot.c
@@ -1604,7 +1604,7 @@ void board_recovery_setup(void)
 	}
 #endif /* CONFIG_AVB_SUPPORT */
 	printf("setup env for recovery..\n");
-	env_set("bootcmd", "run bootcmd_android_recovery");
+	env_set("bootcmd", env_get("bootcmd_android_recovery"));
 }
 #endif /*CONFIG_ANDROID_RECOVERY*/
 #endif /*CONFIG_FSL_FASTBOOT*/