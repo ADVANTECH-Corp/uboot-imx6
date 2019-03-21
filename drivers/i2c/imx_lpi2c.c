nxp-imx/uboot-imx

MLK-16192 lpi2c: Fix bus stop problem in xfer
In xfer function, both bus_i2c_read and bus_i2c_write will
send a STOP command.  This causes a problem when reading register
data from i2c device.

Generally two operations comprise the register data reading:
   1. Write the register address to i2c device.
      START | chip_addr | W | ACK | register_addr | ACK |

   2. Read the Data from i2c device.
      START | chip_addr | R | ACK | DATA          | NACK | STOP

The STOP command should happen at the end of the transfer, otherwise
we will always get data from register address 0

Signed-off-by: Ye Li <ye.li@nxp.com>
Acked-by: Peng Fan <peng.fan@nxp.com>

drivers/i2c/imx_lpi2c.c
@@ -234,9 +234,6 @@ static int bus_i2c_read(struct udevice *bus, u32 chip, u8 *buf, int len)
 	result = bus_i2c_receive(bus, buf, len);
 	if (result)
 		return result;
-	result = bus_i2c_stop(bus);
-	if (result)
-		return result;
 
 	return result;
 }
@@ -249,9 +246,6 @@ static int bus_i2c_write(struct udevice *bus, u32 chip, u8 *buf, int len)
 	if (result)
 		return result;
 	result = bus_i2c_send(bus, buf, len);
-	if (result)
-		return result;
-	result = bus_i2c_stop(bus);
 	if (result)
 		return result;
 
@@ -382,7 +376,7 @@ static int imx_lpi2c_probe_chip(struct udevice *bus, u32 chip,
 
 static int imx_lpi2c_xfer(struct udevice *bus, struct i2c_msg *msg, int nmsgs)
 {
-	int ret = 0;
+	int ret = 0, ret_stop;
 
 	for (; nmsgs > 0; nmsgs--, msg++) {
 		debug("i2c_xfer: chip=0x%x, len=0x%x\n", msg->addr, msg->len);
@@ -399,6 +393,12 @@ static int imx_lpi2c_xfer(struct udevice *bus, struct i2c_msg *msg, int nmsgs)
 
 	if (ret)
 		debug("i2c_write: error sending\n");
+
+	ret_stop = bus_i2c_stop(bus);
+	if (ret_stop)
+		debug("i2c_xfer: stop bus error\n");
+
+	ret |= ret_stop;
 
 	return ret;
 }

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit c8dac8ea2f07c4fb358b824365a1d345bab017fc)

drivers/i2c/imx_lpi2c.c
@@ -229,9 +229,6 @@ static int bus_i2c_read(struct imx_lpi2c_reg *regs, u32 chip, u8 *buf, int len)
 	result = bus_i2c_receive(regs, buf, len);
 	if (result)
 		return result;
-	result = bus_i2c_stop(regs);
-	if (result)
-		return result;
 
 	return result;
 }
@@ -244,9 +241,6 @@ static int bus_i2c_write(struct imx_lpi2c_reg *regs, u32 chip, u8 *buf, int len)
 	if (result)
 		return result;
 	result = bus_i2c_send(regs, buf, len);
-	if (result)
-		return result;
-	result = bus_i2c_stop(regs);
 	if (result)
 		return result;
 
@@ -382,7 +376,7 @@ static int imx_lpi2c_probe_chip(struct udevice *bus, u32 chip,
 static int imx_lpi2c_xfer(struct udevice *bus, struct i2c_msg *msg, int nmsgs)
 {
 	struct imx_lpi2c_reg *regs;
-	int ret = 0;
+	int ret = 0, ret_stop;
 
 	regs = (struct imx_lpi2c_reg *)devfdt_get_addr(bus);
 	for (; nmsgs > 0; nmsgs--, msg++) {
@@ -399,6 +393,12 @@ static int imx_lpi2c_xfer(struct udevice *bus, struct i2c_msg *msg, int nmsgs)
 
 	if (ret)
 		debug("i2c_write: error sending\n");
+
+	ret_stop = bus_i2c_stop(regs);
+	if (ret_stop)
+		debug("i2c_xfer: stop bus error\n");
+
+	ret |= ret_stop;
 
 	return ret;
 }