nxp-imx/uboot-imx

MLK-18129-2 imx8qxp: Add secure boot environment
Add the secure boot relevant environment variables to ARM2 and MEK.
When CONFIG_AHAB_BOOT is enabled, we will switch to boot in this
new way:
1. Load the OS container to address 0x88000000
2. Using auth_cntr to authenticate the OS container. It will load the
   kernel and FDT to destination address.
3. Using booti to boot kernel.

Signed-off-by: Ye Li <ye.li@nxp.com>
Acked-by: Peng Fan <peng.fan@nxp.com>

board/freescale/imx8qxp_arm2/imx8qxp_arm2.c
@@ -647,6 +647,11 @@ int board_late_init(void)
 	setenv("board_rev", "iMX8QXP");
 #endif
 
+	setenv("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	setenv("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

board/freescale/imx8qxp_mek/imx8qxp_mek.c
@@ -612,6 +612,11 @@ int board_late_init(void)
 	setenv("board_rev", "iMX8QXP");
 #endif
 
+	setenv("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	setenv("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

ADVANTECH-Corp/uboot-imx6

(cherry picked from commit 545c8fe112244f20377e97921389c86eacc8551f)

board/freescale/imx8qxp_arm2/imx8qxp_arm2.c
@@ -634,6 +634,11 @@ int board_late_init(void)
 	env_set("board_rev", "iMX8QXP");
 #endif
 
+	env_set("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	env_set("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

board/freescale/imx8qxp_mek/imx8qxp_mek.c
@@ -600,6 +600,11 @@ int board_late_init(void)
 	env_set("board_rev", "iMX8QXP");
 #endif
 
+	env_set("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	env_set("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif